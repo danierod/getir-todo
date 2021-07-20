2083ac3e9884786f8632f8f5cc7df3870de82d5f
Daniel Rodrigues <daniel.v.rodrigues@hotmail.com>
2021-07-21 00:01:18 +0200

    Extend react-testing-library render() with a configured Store Provider

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from './utils/test-utils';
 import App from './App';
 
 test('renders landing page', () => {
   render(<App />);
+
   const titleElement = screen.getByText(/Getir - TODO/i);
+  const todoListElement = screen.getByText(/Todo List/i);
+  const createTodoButton = screen.getByText(/Create Todo/i);
+
   expect(titleElement).toBeInTheDocument();
+  expect(todoListElement).toBeInTheDocument();
+  expect(createTodoButton).toBeInTheDocument();
 });
